bd7e1e82ce7ba103005a31c17c86c9bf8275a321
Gianluca Dias De Micheli <de.micheli.gianluca@gmail.com>
2026-09-10 15:16:20 -0300

    update rds module

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "rds_sg" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [var.eks_security_group_id]
+    security_groups = [var.node_security_group_id]
   }
 
   egress {
